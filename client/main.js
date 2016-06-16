2f2542556dec5bd766cf28c670421fa9b9554a1c
wangyao11 <657316992@qq.com>
2016-06-16 17:13:42 +0800

    增加样式和列表

--- client/main.js
+++ client/main.js
@@ -1,22 +1,29 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
-import './main.html';
-
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+var postsData = [
+  {
+    title: '宇鑫官网',
+    url: 'http://sachagreif.com/introducing-telescope/'
+  }, 
+  {
+    title: '快渔官网',
+    url: 'http://meteor.com'
+  }, 
+  {
+    title: '云商官网',
+    url: 'http://themeteorbook.com'
+  }, 
+  {
+    title: '微站官网',
+    url: 'http://themeteorbook.com'
+  }
+];
+Template.postsList.helpers({
+  posts: postsData
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Template.postItem.helpers({
+  domain: function() {
+    var a = document.createElement('a');
+    a.href = this.url;
+    return a.hostname;
+  }
 });
